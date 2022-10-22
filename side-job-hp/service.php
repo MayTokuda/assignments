c54260ecc04d0b2c6c76dc0e6bc8112c50a36eb6
MayTokuda <mei1984.05.18@gmail.com>
2022-10-23 05:57:07 +0900

    page:service完成

---
+++ side-job-hp/service.php
@@ -0,0 +1,84 @@
+<!-- header読み込み -->
+<?php include "include/header.php" ?>
+
+        <main>
+            <div class="about-us page__wrapper">          
+                <h2 class="page__title" title="事業内容">Service</h2>
+                <img class="page__img"src="img/service__bg.png" alt="">
+
+                <div class="div__inner">
+                    <section id="our-advantage" class="section__container">
+                        <h4 class="section__title" title="私たちの強み">Our Advantage</h4>
+                        <ul class="section__list">
+                            <li class="section__item">
+                                <img class="item__img service__img" src="img/service__advantage__1.png" alt="">
+                                <div class="item__text">
+                                    <h5 class="text__title">テキストテキスト</h5>
+                                    <p class="text__detail">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                    </p>
+                                </div>
+                            </li>
+                            <li class="section__item service__item__reverse">
+                                <img class="item__img service__img" src="img/service__advantage__2.png" alt="">
+                                <div class="item__text">
+                                    <h5 class="text__title">テキストテキスト</h5>
+                                    <p class="text__detail">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+                    <section id="our-feature" class="section__container">
+                        <h4 class="section__title" title="私たちの特徴">Our Feature</h4>
+                        <ul class="section__list service__list">
+                            <li class="section__item">
+                                <img class="item__img service__img" src="img/service__feature__1.png" alt="">
+                                <div class="item__text">
+                                    <h5 class="text__title">テキストテキスト</h5>
+                                    <p class="text__detail">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                    </p>
+                                </div>
+                            </li>
+                            <li class="section__item service__item__reverse">
+                                <img class="item__img service__img" src="img/service__feature__2.png" alt="">
+                                <div class="item__text">
+                                    <h5 class="text__title">テキストテキスト</h5>
+                                    <p class="text__detail">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+
+                <!-- Contact読み込み -->
+                <?php include "include/contact.php" ?>
+            </div>  
+
+        </main>
+
+        <!-- footer読み込み -->
+        <?php include "include/footer.php" ?>
+
+    </body>
+</html>
