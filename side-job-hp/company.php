f3899171f66a1253cbc59c082007d84671c7bc94
MayTokuda <mei1984.05.18@gmail.com>
2022-10-23 11:03:35 +0900

    page:company完成

---
+++ side-job-hp/company.php
@@ -0,0 +1,64 @@
+<!-- header読み込み -->
+<?php include "include/header.php" ?>
+
+        <main>
+            <div class="company page__wrapper">          
+                <h2 class="page__title" title="会社概要">Company</h2>
+                <img class="page__img" src="img/company__bg.png" alt="">
+
+                <div class="div__inner">
+                    <dl class="company__info">
+                        <dt>会社名</dt>
+                        <dd>株式会社テックコーポレート</dd>
+                        <dt>設立</dt>
+                        <dd>20XX年X月X日</dd>
+                        <dt>代表者</dt>
+                        <dd>テック 太郎</dd>
+                        <dt>資本金</dt>
+                        <dd>5,000,000万円</dd>
+                        <dt>従業員数</dt>
+                        <dd>15名</dd>
+                        <dt>事業内容</dt>
+                        <dd>テキストテキストテキスト<br>
+                            テキストテキストテキスト</dd>
+                        <dt>住所</dt>
+                        <dd>〒123-1234<br>
+                            東京都東京区東京町1-1-1</dd>
+                        <dt>お問い合わせ</dt>
+                        <dd>Tel：080–1234-5678<br>
+                            Mail：xxx@xxx.xx</dd>
+                    </dl>
+                    <div class="iframe-wrap">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.699861976722!2d139.76494800121125!3d35.68122342874577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1666490453153!5m2!1sja!2sjp" width="1080" height="480" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <section id="our-advantage" class="section__container">
+                        <h4 class="section__title" title="社長挨拶">Message</h4>
+                            <div class="section__item company__item">
+                                <div class="item__text">
+                                    <h5 class="text__title">テキストテキスト</h5>
+                                    <p class="text__detail">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            テキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                                                            <br>
+                                                            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                    </p>
+                                </div>
+                                <img class="item__img company__img" src="img/１.png" alt="">
+                            </div>
+                    </section>
+
+                </div>
+
+                <!-- Contact読み込み -->
+                <?php include "include/contact.php" ?>
+            </div>  
+
+        </main>
+
+        <!-- footer読み込み -->
+        <?php include "include/footer.php" ?>
+
+    </body>
+</html>
